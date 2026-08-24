7983ff619970df7f9ed124e1093ed394446cd625
Pranay <lmacherla123@gmail.com>
2026-08-24 20:29:40 +0530

    Update database.php

--- database.php
+++ database.php
@@ -2,11 +2,10 @@
 // InfinityFree database connection (default)
 // You can override any of these using environment variables:
 // DB_HOST, DB_USER, DB_PASS, DB_NAME
-$servername = getenv('DB_HOST') ?: "sql308.infinityfree.com";
-$username = getenv('DB_USER') ?: "if0_41188596";
-$password = getenv('DB_PASS') ?: "L2QC39aV6LRxD";
-$database = getenv('DB_NAME') ?: "if0_41188596_aura_streams";
-
+$servername = getenv('DB_HOST');
+$username   = getenv('DB_USER');
+$password   = getenv('DB_PASS');
+$database   = getenv('DB_NAME');
 // Local XAMPP config (reference):
 // $servername = "localhost";
 // $username = "root";
